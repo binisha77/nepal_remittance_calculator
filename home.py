4c83edb137c951f0c90a039a41cca1aafd7a8d57
Binisha Khadka <khadkabinisha48@gmail.com>
2026-03-11 08:29:27 +0545

    feat: receiver details

--- home.py
+++ home.py
@@ -1,1 +1,13 @@
 
+# Program to take receiver details
+
+# Taking input from user
+receiver_name = input("Enter receiver's name: ")
+country = input("Enter receiver's country: ")
+amount = float(input("Enter amount to send: "))
+
+# Displaying the information
+print("\n--- Transaction Details ---")
+print("Receiver Name:", receiver_name)
+print("Country:", country)
+print("Amount:", amount)
