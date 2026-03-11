910919ef5bd20ec37ed5f289dae19696f33d7070
subhikshya shretha <subhikshyashrestha@gmail.com>
2026-03-11 16:15:28 +0545

    Task3 : Calculate NPR amount and service charge

--- home.py
+++ home.py
@@ -11,3 +11,21 @@ print("\n--- Transaction Details ---")
 print("Receiver Name:", receiver_name)
 print("Country:", country)
 print("Amount:", amount)
+
+
+# Task 3: Calculate NPR amount and service charge
+
+if country.lower() == "usa":
+    rate = 132
+elif country.lower() == "india":
+    rate = 1.6
+elif country.lower() == "australia":
+    rate = 88
+else:
+    rate = 100  # default rate
+
+npr_amount = amount * rate
+service_charge = npr_amount * 0.02
+
+print("Converted NPR:", npr_amount)
+print("Service Charge:", service_charge)
